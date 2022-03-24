68ea7e7d6177908d55d8bc4602d780ac4168fed3
Soraya <soraya_mahmoudi95@yahoo.com>
2022-03-24 20:11:06 +0430

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,38 @@
+function formatDate(timeStamp)
+{
+    let date=new Date(timeStamp);
+    let hourse=date.getHours();
+    if(hourse<10){hourse=`0${hourse}`;}
+    let minutes=date.getMinutes();
+    if(minutes<10){minutes=`0${minutes}`;}
+    let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+    let day=days[date.getDay()];
+    let year=date.getFullYear();
+    let month=date.getMonth()+1;  
+  if (month<10){
+    month=`0${month}`;
+  }
+  
+  let numDay=date.getDate();
+  if(numDay<10){
+    numDay=`0${numDay}`;
+  }
+  let nowDate=document.querySelector("#nowDate");
+    
+      let formatDate = ` ${year}-${month}-${numDay}`;
+      nowDate.innerHTML=formatDate; 
+    console.log(year);
+    return `${day} ${hourse}: ${minutes}`;
+}
+
 function displayTemperature(response){
     console.log(response.data);
     let temperatureElement=document.querySelector("#temperature");
@@ -14,6 +49,9 @@ function displayTemperature(response){
 
     let windElement=document.querySelector("#wind");
     windElement.innerHTML=Math.round(response.data.wind.speed);
+
+    let dateElement=document.querySelector("#date");
+    dateElement.innerHTML=formatDate(response.data.dt*1000);
 
 }
 
